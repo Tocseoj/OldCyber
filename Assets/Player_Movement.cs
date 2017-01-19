def7541dd257980c74442f326f8f289991dbc62a
Joseph Marshall <marsha87@purdue.edu>
2017-01-18 19:32:03 -0500

    First Commit.

---
+++ Assets/Player_Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+	public float speedModifier = 0.1f;
+
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Awake () {
+		rigidBody = transform.GetComponent<Rigidbody2D> ();
+		if (!rigidBody) Debug.Log("rigidBody not defined!");
+	}
+	
+	void FixedUpdate() {
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+		//Vector2 moveTo = new Vector2 (moveX * speedModifier, moveY * speedModifier);
+		//rigidBody.MovePosition (rigidBody.position + moveTo * Time.fixedDeltaTime);
+		rigidBody.AddForce(new Vector2(moveX * speedModifier * Time.fixedDeltaTime, moveY * speedModifier * Time.fixedDeltaTime));
+	}
+}
---
+++ Assets/Turret.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+	public float fireRate = 0;
+	public float Damage = 10;
+	public float knockback = 1;
+
+	public LayerMask toHit;
+	public Transform player;
+
+	float timeToFire;
+
+	void Awake() {
+		timeToFire = Random.value;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.right = player.position - transform.position;
+
+		if (fireRate != 0 && Time.time > timeToFire) {
+			timeToFire = Time.time + 1/(fireRate + Random.Range(-0.1f, 0.1f));
+			Shoot ();
+		}
+	}
+
+	void Shoot() {
+		Vector2 firePoint = new Vector2 (transform.position.x, transform.position.y);
+		RaycastHit2D hit = Physics2D.Raycast (firePoint, transform.right, 100f, toHit);
+		Debug.DrawRay (firePoint, transform.right * 100f, Color.yellow);
+		if (hit.collider != null) {
+			Debug.DrawLine (firePoint, hit.point, Color.red);
+			hit.rigidbody.AddForceAtPosition (transform.right * knockback, hit.point);
+		}
+	}
+}
