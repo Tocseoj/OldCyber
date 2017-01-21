32b546f24e9c9f96803870218c71a7716c7e19ed
Joseph Marshall <marsha87@purdue.edu>
2017-01-21 03:45:57 -0500

    wow

--- Assets/Scripts/Player_Animation.cs
+++ Assets/Scripts/Player_Animation.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Player_Animation : MonoBehaviour {
 
+	// Public Var
+	public RuntimeAnimatorController idleAnimator;
+	public RuntimeAnimatorController weaponAnimator;
+
+	// Private var
+	bool weaponOut = false;
+
 	// References
 	Animator anim;
 	Player_Movement pm;
 	// Use this for initialization
 	void Awake () {
+		if (weaponAnimator == null) {
+			Debug.Log ("No RuntimeWeaponAnimator found on " + gameObject.name);
+			this.enabled = false;
+		}
+		if (idleAnimator == null) {
+			Debug.Log ("No RuntimeIdleAnimator found on " + gameObject.name);
+			this.enabled = false;
+		}
 		anim = GetComponent<Animator> ();
 		if (anim == null) {
 			Debug.LogError ("No Animator found on " + gameObject.name);
@@ -24,5 +39,14 @@ public class Player_Animation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		anim.SetInteger ("direction", pm.GetDirection ());
+		if (Input.GetButtonDown ("Fire2")) {
+			if (weaponOut) {
+				anim.runtimeAnimatorController = idleAnimator;
+				weaponOut = false;
+			} else {
+				anim.runtimeAnimatorController = weaponAnimator;
+				weaponOut = true;
+			}
+		}
 	}
 }
