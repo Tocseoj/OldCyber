9063a7c37f1c8a1772758e3ab130f6bbf77f46ec
Joseph Marshall <marsha87@purdue.edu>
2017-02-06 22:20:03 -0500

    camera res

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,17 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
+
+	int ppu = 8;
 
 	// Use this for initialization
 	void Awake () {
 		DontDestroyOnLoad (gameObject);
-		Camera.main.orthographicSize = Screen.height / 2;
+		Debug.Log (Screen.height);
+		//Camera.main.orthographicSize = (float)Screen.height / 8.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+		if (Input.GetKeyDown (KeyCode.R)) {
+			SceneManager.LoadScene (0);
+		}
+	}
+
+	public void ChangePPU(float _ppu) {
+		ppu = (int)_ppu;
+	}
+
+	public void ReloadScene() {
+		Camera.main.orthographicSize = (float)Screen.height / ppu / 2;
 	}
 }
